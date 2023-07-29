d99efb92ff5ae463b4a26d9175682666defe421c
Pritam Behera <140886260+pritambehera02@users.noreply.github.com>
2023-07-29 19:58:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+function talk() {
+    var know = {
+        "who are you": "Hello, Chatbot is here for you ",
+        "hii" : "Heyyy, What's Up ???",
+        "how are you": "Good :)",
+        "what can you do for me": "Everything :) Just say to me >>Thank Me Later",
+        "Your followers":
+            "I have my family of 200000 members, i don't have follower ,have supportive Famiy ",
+        ok: "Thank You So Much ",
+        bye: "Okay! Will meet soon..",
+    };
+    var user = document.getElementById("userBox").value;
+    document.getElementById("chatLog").innerHTML = user + "<br>";
+    if (user in know) {
+        document.getElementById("chatLog").innerHTML = know[user] + "<br>";
+    } else {
+        document.getElementById("chatLog").innerHTML =
+            "Sorry,I didn't understand <br>";
+    }
+}
+
