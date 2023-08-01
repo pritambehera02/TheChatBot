17468a3b7db2c8a80fbbcc5e077c0d1b84a2424e
Pritam Behera <140886260+pritambehera02@users.noreply.github.com>
2023-08-01 11:52:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ function talk() {
     var know = {
         "who are you": "Hello, Chatbot is here for you ",
         "hii" : "Heyyy, What's Up ???",
-        "preeti": "Oyeee Preetu, Thanks for being my Best Friend. I Always will be there for you",
-        "Preeti": "Oyeee Preetu, Thanks for being my Best Friend. I Always will be there for you",
+        "Hii": "Heyyy, Tell me something",
+        "Nothing":"Okay as your wish",
         "how are you": "Good :)",
         "what can you do for me": "Everything :) Just say to me >>Thank Me Later",
         "Your followers":
